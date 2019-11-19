e5416388e31144ad6b1d32a5657a4fb70241083e
Armel <armelfli50@gmail.com>
2019-11-19 00:25:42 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+let result =  array.find(e =>{
+    if(e["result"] === "W"){
+      return e
+    }
+  })
+  if(result){return result.year}
+}
