0b0f2597aad465fafee3cfe08f9f4d3221467141
KHong <albtrossfacefall@gmail.com>
2019-12-03 14:52:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+function superbowlWin(array) {
+  const winner = array.find(function(o) {
+    return o.result === "W";
+  });
+  return winner && winner.year;
 }
