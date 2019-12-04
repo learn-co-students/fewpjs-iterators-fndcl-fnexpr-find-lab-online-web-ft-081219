e710ff22b53faf6286a1ef09f65233e9caeeb290
Caleb Kavanaugh <calebck@hotmail.com>
2019-12-03 19:25:01 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const win = array.find(object => object.result === "W")
+  if (win) {
+    return win.year
+  }
+}
